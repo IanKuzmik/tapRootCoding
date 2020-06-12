ceea1d70a7c3d194ec1a6d0c6985d46c59a2b3bd
IanKuzmik <56178373+IanKuzmik@users.noreply.github.com>
2020-06-12 15:57:48 -0600

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,53 @@
+
+//sticky header
+const stickyHeader = document.getElementById('js-sticky-header');
+document.addEventListener( 'scroll', function() {
+  if ( window.pageYOffset > 300 ) {
+    stickyHeader.classList.add('reveal');
+  } else {
+    stickyHeader.classList.remove('reveal');
+  }
+});
+
+//profile pic hover effect
+const profilePic = document.getElementById('js-bio-profile-picture');
+const profilePicOverlay = document.getElementById('js-profile-picture-overlay');
+profilePicOverlay.addEventListener( 'mouseover', function() {
+  profilePicOverlay.classList.add('animate');
+  profilePic.classList.add('animate');
+});
+profilePicOverlay.addEventListener("mouseout", function() {
+  profilePicOverlay.classList.remove('animate');
+  profilePic.classList.remove('animate');
+});
+
+//profile pic header
+const headerProfilePic = document.getElementById('js-header-profile-picture');
+window.addEventListener( 'scroll', function() {
+  let ele = profilePic.getBoundingClientRect();
+  if ( (ele.y + ele.height) < 0 ) {
+    headerProfilePic.classList.add('reveal');
+  } else {
+    headerProfilePic.classList.remove('reveal');
+  }
+});
+
+
+//card slide into view
+const cards = document.querySelectorAll('.my-services .card');
+const observerOptions = {
+  threshold  : 0,
+  rootMargin : '0px 0px -250px 0px'
+};
+const cardObserver = new IntersectionObserver( (entries, imgObserver) => {
+  entries.forEach( entry => {
+    if ( !entry.isIntersecting ) return;
+    let list = entry.target.parentElement.querySelector('.list');
+    entry.target.setAttribute( 'style', 'transform: translateX(0%); opacity: 1;');
+    setTimeout( function() { list.style.opacity = 1; }, 800 );
+    imgObserver.unobserve(entry.target);
+  })
+}, observerOptions );
+cards.forEach( (image) => {
+  cardObserver.observe(image);
+});
