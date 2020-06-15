81a82fd42454913f0290671cf83aa761ae2b0262
Ian <kuzmik951@gmail.com>
2020-06-15 14:32:08 -0600

    made everything responsive

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 
-const stickyHeader      = document.getElementById('js-sticky-header');
-const headerProfilePic  = document.getElementById('js-header-profile-picture');
-const profilePic        = document.getElementById('js-bio-profile-picture');
-const cards             = document.querySelectorAll('.my-services .card');
+const stickyHeader        = document.getElementById('js-sticky-header');
+const headerProfilePic    = document.getElementById('js-header-profile-picture');
+const profilePicContainer = document.querySelector('.profile-picture-container');
+const profilePic          = document.getElementById('js-bio-profile-picture');
+const quickBioText        = document.querySelector('.quick-bio-text');
+const cards               = document.querySelectorAll('.my-services .card');
+const resumeExperience    = document.querySelector('.resume .experience');
+const resumeInformation   = document.querySelector('.resume .information');
 
 //sticky header
 document.addEventListener( 'scroll', function() {
@@ -40,3 +44,41 @@ const cardObserver = new IntersectionObserver( (entries, imgObserver) => {
 cards.forEach( (image) => {
   cardObserver.observe(image);
 });
+
+//responsive 
+function isBreakPoint( breakPoint, specific = false, breakPoints = [576, 768, 992] ) {
+  let screenWidth = window.innerWidth;
+  if ( specific ) {
+    let min, max;
+    for ( i = 0; i < breakPoints.length; i++ ) {
+      if ( breakPoints[i] == breakPoint ) {
+        min = breakPoints[i-1] || 0;
+        max = breakPoints[i];
+        break;
+      }
+    }
+    // returns 'true' if screenWidth is between breakPoint and the next smallest breakPoint
+    return (screenWidth > min && screenWidth <= max);
+  }
+  // returns 'true' if screenWidth is less than breakPoint i.e. the breakPoint has been hit
+  else return breakPoint >= screenWidth;
+} 
+function checkBreakpoints() {
+  if ( isBreakPoint(768) ) {
+    quickBioText.setAttribute( 'data-col', '10');
+    profilePicContainer.setAttribute( 'data-col', '4');
+    resumeExperience.setAttribute( 'data-col', '12');
+    resumeInformation.setAttribute( 'data-col', '12');
+  }
+  else if ( isBreakPoint(992) ) {
+    profilePicContainer.setAttribute( 'data-col', '3');
+    quickBioText.setAttribute( 'data-col', '8');
+    resumeExperience.setAttribute( 'data-col', '9');
+    resumeInformation.setAttribute( 'data-col', '3');
+  } else {
+    profilePicContainer.setAttribute( 'data-col', '4');
+    quickBioText.setAttribute( 'data-col', '6');
+  }
+}
+checkBreakpoints(); 
+window.addEventListener( 'resize', checkBreakpoints );
